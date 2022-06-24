f27933d4d23a26b9885a528b506790ce77ce21b9
Ahmed Safwat <ahmedbody866@gmail.com>
2022-06-24 00:17:33 -1200

    added upload user image option

--- assets/js/userInfo.js
+++ assets/js/userInfo.js
@@ -1,5 +1,24 @@
 document.querySelectorAll("input").forEach((input) => {
   input.addEventListener("change", () => {
-    document.querySelector(".change").classList.remove("!hidden")
-  })
-})
+    document.querySelector(".change").classList.remove("!hidden");
+  });
+});
+
+if (localStorage.getItem("loadedImage")) {
+  $(".user-image").attr("src", JSON.parse(localStorage.getItem("loadedImage")));
+}
+
+$(".camera-file-upload").on("click", function () {
+  document.querySelector(".upload-user-img").click();
+});
+
+$(".upload-user-img").on("change", function () {
+  const reader = new FileReader();
+
+  reader.onload = function () {
+    $(".user-image").attr("src", reader.result);
+    localStorage.setItem("loadedImage", JSON.stringify(reader.result));
+  };
+
+  reader.readAsDataURL(this.files[0]);
+});
